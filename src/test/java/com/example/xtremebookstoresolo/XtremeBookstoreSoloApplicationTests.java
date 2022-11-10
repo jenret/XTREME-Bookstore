95497a6b6f1a060c4413a8485f1da9d3596bc1dd
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2022-11-09 20:06:11 -0700

    testing testing

---
+++ src/test/java/com/example/xtremebookstoresolo/XtremeBookstoreSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xtremebookstoresolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtremeBookstoreSoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
