459b9c7474904724f4d8996abaf6cd43ea2e082d
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2022-11-15 19:34:13 -0700

    Book model fleshed out, some javascript to retrieve books

--- src/main/java/com/example/xtremebookstore/data/BookBLL.java
+++ src/main/java/com/example/xtremebookstore/data/BookBLL.java
@@ -1,4 +1,7 @@
 package com.example.xtremebookstore.data;
 
 public class BookBLL {
+
+    String url = "xtreme-bookstore.cv9cd3sfovyv.us-west-2.rds.amazonaws.com";
+
 }
--- src/main/java/com/example/xtremebookstore/models/BookModel.java
+++ src/main/java/com/example/xtremebookstore/models/BookModel.java
@@ -1,5 +1,72 @@
 package com.example.xtremebookstore.models;
 
+import java.util.Date;
+
 public class BookModel {
-    //omg it,s a book ISBN, author, publish date, edition, and purchase price.
+    public int ISBN;
+    private String title;
+    private String author;
+    private Date publishDate;
+    private String edition;
+    private double purchasePrice;
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public BookModel(int ISBN, String title, String author, Date publishDate, String edition, double purchasePrice) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.publishDate = publishDate;
+        this.edition = edition;
+        this.purchasePrice = purchasePrice;
+    }
+
+    public BookModel() {
+    }
 }
