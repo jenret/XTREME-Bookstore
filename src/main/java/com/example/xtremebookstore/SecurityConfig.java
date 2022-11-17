2d37990b7b9f5536ad24c7e3f4685227f21d51ec
johnsonisaacpeter <ijohnson@student.neumont.edu>
2022-11-16 19:21:15 -0700

    Basic security added with one in memory user

--- src/main/java/com/example/xtremebookstore/SecurityConfig.java
+++ src/main/java/com/example/xtremebookstore/SecurityConfig.java
@@ -2,28 +2,62 @@ package com.example.xtremebookstore;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) {
-
-    }
+    private final InMemoryUserDetailsManager authMem = new InMemoryUserDetailsManager();
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails superAdmin = User.withUsername("superAdmin")
+                .password(passwordEncoder().encode("superAdmin"))
+                .roles("ADMIN")
+                .build();
+
+        authMem.createUser(superAdmin);
+        return authMem;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, UserDetailsService userDetailsService) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .httpBasic(Customizer.withDefaults())
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+
 }
