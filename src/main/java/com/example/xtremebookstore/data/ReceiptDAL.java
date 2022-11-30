a396d13d58e7b5ce23b930582c6d3250f040ea43
CynthiaLlamas <98438989+CynthiaLlamas@users.noreply.github.com>
2022-11-29 20:07:25 -0700

    getReceiptsFromCurrentMonth

--- src/main/java/com/example/xtremebookstore/data/ReceiptDAL.java
+++ src/main/java/com/example/xtremebookstore/data/ReceiptDAL.java
@@ -49,5 +49,31 @@ public class ReceiptDAL {
         }
         return receipts;
     }
+    //Get Receipts by month
+    public ArrayList<ReceiptModel> getReceiptsFromCurrentMonth(){
+        String sql = "SELECT * FROM `XTreme-Bookstore`.receipts_view\n" +
+                "WHERE monthname(timeOfSale) LIKE monthname(current_timestamp) " +
+                "and year(timeOfSale) = year(current_timestamp())\n" +
+                "GROUP BY extract(YEAR from timeOfSale), extract(MONTH from timeOfSale)\n" +
+                "order by timeOfSale";
+        ArrayList<ReceiptModel> receipts = new ArrayList<>();
+        try{
+            Connection con  = DriverManager.getConnection(url,user,password);
+            PreparedStatement pst = con.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()){
+                receipts.add(new ReceiptModel(rs.getInt(1)
+                        ,rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getString(5),
+                        rs.getInt(6),
+                        rs.getString(7),
+                        rs.getDouble(8),
+                        rs.getTimestamp(9).toLocalDateTime()));
+            }
+        }catch (SQLException sqle){sqle.printStackTrace();}
+        return receipts;
+    }
 
 }
