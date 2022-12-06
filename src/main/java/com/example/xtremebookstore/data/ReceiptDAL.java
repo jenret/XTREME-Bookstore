0574456052d58e5cdd83ff26427e4b9e498ca752
johnsonisaacpeter <ijohnson@student.neumont.edu>
2022-12-06 12:30:42 -0700

    updated for using the store name when making a user

--- src/main/java/com/example/xtremebookstore/data/ReceiptDAL.java
+++ src/main/java/com/example/xtremebookstore/data/ReceiptDAL.java
@@ -1,9 +1,6 @@
 package com.example.xtremebookstore.data;
 
-import com.example.xtremebookstore.models.ReceiptModel;
-import com.example.xtremebookstore.models.SalesPerAuthor;
-import com.example.xtremebookstore.models.SalesPerBook;
-import com.example.xtremebookstore.models.SalesPerStore;
+import com.example.xtremebookstore.models.*;
 
 import java.sql.*;
 import java.time.LocalDateTime;
@@ -30,16 +27,35 @@ public class ReceiptDAL {
             e.printStackTrace();
         }
     }
-    public static ArrayList<ReceiptModel> getAllReceipts(){
+
+    public static void createReceipts(ArrayList<ReceiptModel> receiptModels) {
+        String insert = "insert into sales (bookID, userID, storeID, salePrice) values (?, ?, ?, ?)";
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement pst = con.prepareStatement(insert);
+            for (ReceiptModel receipt : receiptModels) {
+                pst.setString(1, receipt.getBookISBN());
+                pst.setInt(2, receipt.getUserID());
+                pst.setInt(3, receipt.getStoreID());
+                pst.setDouble(4, receipt.getSalePrice());
+                pst.addBatch();
+            }
+            pst.executeBatch();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<ReceiptModel> getAllReceipts() {
         String sql = "SELECT * FROM receipts_view";
         ArrayList<ReceiptModel> receipts = new ArrayList<>();
-        try{
-            Connection con = DriverManager.getConnection(url,user,password);
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
             PreparedStatement pst = con.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 receipts.add(new ReceiptModel(rs.getInt(1)
-                        ,rs.getString(2),
+                        , rs.getString(2),
                         rs.getString(3),
                         rs.getInt(4),
                         rs.getString(5),
@@ -48,26 +64,27 @@ public class ReceiptDAL {
                         rs.getDouble(8),
                         rs.getTimestamp(9).toLocalDateTime()));
             }
-        }catch (SQLException sqle){
+        } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
         return receipts;
     }
+
     //Get Receipts by month
-    public static ArrayList<ReceiptModel> getReceiptsFromCurrentMonth(){
+    public static ArrayList<ReceiptModel> getReceiptsFromCurrentMonth() {
         String sql = "SELECT * FROM `XTreme-Bookstore`.receipts_view\n" +
                 "WHERE monthname(timeOfSale) LIKE monthname(current_timestamp) " +
                 "and year(timeOfSale) = year(current_timestamp())\n" +
                 "GROUP BY extract(YEAR from timeOfSale), extract(MONTH from timeOfSale)\n" +
                 "order by timeOfSale";
         ArrayList<ReceiptModel> receipts = new ArrayList<>();
-        try{
-            Connection con  = DriverManager.getConnection(url,user,password);
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
             PreparedStatement pst = con.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 receipts.add(new ReceiptModel(rs.getInt(1)
-                        ,rs.getString(2),
+                        , rs.getString(2),
                         rs.getString(3),
                         rs.getInt(4),
                         rs.getString(5),
@@ -76,24 +93,27 @@ public class ReceiptDAL {
                         rs.getDouble(8),
                         rs.getTimestamp(9).toLocalDateTime()));
             }
-        }catch (SQLException sqle){sqle.printStackTrace();}
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
         return receipts;
     }
-    public static ArrayList<ReceiptModel> getReceiptsFromAMonth(String month){
+
+    public static ArrayList<ReceiptModel> getReceiptsFromAMonth(String month) {
         String sql = "SELECT * FROM `XTreme-Bookstore`.receipts_view\n" +
                 "WHERE monthname(timeOfSale) LIKE ? " +
                 "and year(timeOfSale) = year(current_timestamp())\n" +
                 "GROUP BY extract(YEAR from timeOfSale), extract(MONTH from timeOfSale)\n" +
                 "order by timeOfSale";
         ArrayList<ReceiptModel> receipts = new ArrayList<>();
-        try{
-            Connection con = DriverManager.getConnection(url,user,password);
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
             PreparedStatement pst = con.prepareStatement(sql);
-            pst.setString(1,"%"+month+"%");
+            pst.setString(1, "%" + month + "%");
             ResultSet rs = pst.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 receipts.add(new ReceiptModel(rs.getInt(1)
-                        ,rs.getString(2),
+                        , rs.getString(2),
                         rs.getString(3),
                         rs.getInt(4),
                         rs.getString(5),
@@ -102,13 +122,10 @@ public class ReceiptDAL {
                         rs.getDouble(8),
                         rs.getTimestamp(9).toLocalDateTime()));
             }
-        }catch (SQLException sqle){
+        } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
         return receipts;
-    }
-    public void getTotalSalesByBook(){
-
     }
 
     public static ArrayList<SalesPerBook> getBookSalesPerMonth(int month) {
@@ -135,7 +152,7 @@ public class ReceiptDAL {
 
     public static ArrayList<SalesPerStore> getStoreSalesPerMonth(int month) {
         String query = "select storeName, sum(salePrice) as salesTotal, count(salesID) as numberOfSales " +
-                "from receipts_view month(timeOfSale) = ? and year(timeOfSale) = year(now()) group by title";
+                "from receipts_view where month(timeOfSale) = ? and year(timeOfSale) = year(now()) group by storeName";
         try {
             Connection con = DriverManager.getConnection(url, user, password);
             PreparedStatement pst = con.prepareStatement(query);
@@ -173,6 +190,26 @@ public class ReceiptDAL {
             return results;
         } catch (Exception e) {
             System.out.println("Error getAuthorSalesPerMonth");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<SalesPerStorePerMonth> getStoreSalesEachMonth() {
+        String query = "";
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement pst = con.prepareStatement(query);
+            ResultSet rs = pst.executeQuery();
+            ArrayList<SalesPerStorePerMonth> results = new ArrayList<>();
+            while(rs.next()) {
+                results.add(new SalesPerStorePerMonth(rs.getString("storeName"),
+                        rs.getInt("month"),
+                        rs.getDouble("salesTotal")));
+            }
+            return results;
+        } catch (Exception e) {
+            System.out.println("Error getStoreSalesEachMonth");
             e.printStackTrace();
             return null;
         }
