38bb488205c0601db6357da74b61cd6555339744
johnsonisaacpeter <ijohnson@student.neumont.edu>
2022-12-06 18:46:23 -0700

    More book fixes

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -94,7 +94,7 @@ function addBook() {
     var object = {
         "ISBN": ISBN, //strings work fine
         "title": title, //string
-        "authorName": author, //int works fine
+        "author": author, //int works fine
         "publishDate": publish, //SQL date
         "edition": edition, //int
         "purchasePrice": price //double
